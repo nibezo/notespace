3108d816052159edb0d32a6c68fb63effb3b9aa5
Ilya Yudin <nibezo.cs@gmail.com>
2023-01-15 23:32:25 +0300

    feat: password input html

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  const [data, setData] = React.useState(null);
-  
-  React.useEffect(() => {
-    fetch("/api")
-    .then((res) => res.json())
-    .then((data) => setData(data.message))
-  })
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello!</h1>
-        <p>
-          {!data ? "Loading..." : data}
-        </p>
-      </header>
+    <div className="app">
+      <div className="input-element-wrapper">
+        <input type="password" placeholder="Enter Password"></input>
+        <button>
+          <img src={require('./img/close-eye.png')}/>
+        </button>
+      </div>
     </div>
-  );
-}
+  )
+};
 
-export default App;
+export default App; 
