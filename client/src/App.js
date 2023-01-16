96a09b2214b70d8e9370caf8860de5e651badd78
Ilya Yudin <nibezo.cs@gmail.com>
2023-01-16 13:54:36 +0300

    feat: add password input with setState

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,28 @@
+import './style.css';
+import { useState } from 'react';
+
 const App = () => {
+  const [state, setState] = useState(false);
+
+  const toggleBtn = () => {
+    setState((prevState) => !prevState);
+  };
+
   return (
     <div className="app">
       <div className="input-element-wrapper">
-        <input type="password" placeholder="Enter Password"></input>
-        <button>
-          <img src={require('./img/close-eye.png')}/>
+        <input
+          className="password-field"
+          type={state ? "text" : "password"}
+          placeholder="password"
+          autoComplete='off'
+        ></input>
+        <button className="btn" onClick={toggleBtn}>
+          <img src={state ? require('./img/open-eye.png') : require('./img/close-eye.png') } />
         </button>
       </div>
     </div>
-  )
+  );
 };
 
-export default App; 
+export default App;
